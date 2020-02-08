01255f942358bb1f2ae6e56e3b9b534e26ac5a67
Rodion Horbulenko <Rodion.Gorbulenko@gmail.com>
2020-02-08 15:50:01 +0200

    Asynchronous methods removed, refactoring done

---
+++ src/constants/httpCodes.constant.js
@@ -0,0 +1,3 @@
+exports.BAD_REQUEST = 400;
+exports.FORBIDDEN = 403;
+exports.EXPIRED = 410;
--- src/constants/httpCodes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  BAD_REQUEST: 400,
-  FORBIDDEN: 403,
-  EXPIRED: 410,
-  NOT_FOUND: 404
-};
